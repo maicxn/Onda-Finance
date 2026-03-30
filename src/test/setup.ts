90ffc14b6e9bb3b44c5372acf44b75896de019c9
maicxn <maiconkelbert@gmail.com>
2026-03-30 00:56:20 -0300

    test: polyfill PointerEvent capture for Radix Select in jsdom

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,23 @@
 import '@testing-library/jest-dom/vitest'
+
+// Polyfills for Radix UI PointerEvents in jsdom
+if (typeof window !== 'undefined') {
+  if (!window.Element.prototype.hasPointerCapture) {
+    window.Element.prototype.hasPointerCapture = function () {
+      return false
+    }
+  }
+  if (!window.Element.prototype.setPointerCapture) {
+    window.Element.prototype.setPointerCapture = function () {}
+  }
+  if (!window.Element.prototype.releasePointerCapture) {
+    window.Element.prototype.releasePointerCapture = function () {}
+  }
+  // Mock ResizeObserver
+  class ResizeObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  }
+  window.ResizeObserver = ResizeObserver
+}
